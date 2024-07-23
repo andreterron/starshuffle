51cdf934592fa8c704a5cc638fcdee5d9a9920ae
Andre Terron <git@terron.me>
2024-07-22 18:32:28 -0700

    Setup Mainframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useConnection, useRequest } from "@mainframe-api/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { connection, initiateAuth } = useConnection("github");
+
+  // This makes a request to the GitHub API through a Mainframe proxy
+  const { data } = useRequest(connection, "/user");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Mainframe</h1>
+      <button onClick={() => initiateAuth()}>
+        {connection ? "Connected ✓" : "Connect to GitHub"}
+      </button>
+      {data && <p>Connected as @{data.login}</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { MainframeProvider } from "@mainframe-api/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MainframeProvider appId="d0174965d74a823a71af48d5d88dc9dc">
+      <App />
+    </MainframeProvider>
   </React.StrictMode>,
-)
+);
