c01554a415599f571ad7901a5f99a0fd9e78e4cf
Andre Terron <git@terron.me>
2024-07-24 22:37:37 -0700

    Update auth

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { MainframeProvider } from "@mainframe-api/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MainframeProvider appId={import.meta.env.MAINFRAME_APP_ID}>
+    <MainframeProvider appId={import.meta.env.VITE_MAINFRAME_APP_ID}>
       <App />
     </MainframeProvider>
   </React.StrictMode>,
