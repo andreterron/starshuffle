1b22bdc8d2c8ee79e5b92feacb0e34fb673bc1c6
Andre Terron <git@terron.me>
2024-07-29 14:45:55 -0700

    Add Posthog

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import { MainframeProvider } from "@mainframe-api/react";
 import { ThemeProvider } from "./components/ui/theme-provider.tsx";
+import posthog from "posthog-js";
+
+posthog.init("phc_5xv7jae6Br2ZYtwI1FbvX2MvMkvryfBYTAzamEtBCR6", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
